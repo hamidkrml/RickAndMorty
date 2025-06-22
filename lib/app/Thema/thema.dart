0a3733383fa73ce044e0582aa604a53cf5fefe37
hamidkrml <karimlihamid4@gmail.com>
2025-06-22 19:44:37 +0300

    create AppThema with light theme

---
+++ lib/app/Thema/thema.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppThema {
+  AppThema._();
+
+  /// disiradin nesne olustura bilmesin  dege private yapmak icin  AppThema._(); kullanilir
+
+  static ThemeData get lightTheme => ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+
+    colorScheme: const ColorScheme.light(
+      primary: Color(0xFF42BACA),
+      secondary: Color(0xFF42BACA),
+      surface: Colors.white,
+      onSurface: Color(0xFF414A4C),
+      error: Color(0xFF414A4C),
+      tertiary: Color(0xFFB5C4C7),
+    ),
+  );
+}
