ad946910568d4842f4e820a22d20d13fe7edf403
hamidkrml <karimlihamid4@gmail.com>
2025-07-03 12:31:21 +0400

    refactor KarakterView to include a search input widget and a character listing area

--- lib/app/View/Karakter_View/karakter_view.dart
+++ lib/app/View/Karakter_View/karakter_view.dart
@@ -6,11 +6,45 @@ class KarakterView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Karakterler'),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18.0),
+          child: Column(
+            children: [
+              // Burada karakterlerin arama yapacagi textfield
+              const SizedBox(height: 12), _searchinputWidget(context),
+
+              // Burada karakterlerin listeleneceği alan
+              // Container(
+              //   height: 200.0,
+              //   width: double.infinity,
+              //   decoration: BoxDecoration(
+              //     color: Theme.of(context).colorScheme.secondary,
+              //     borderRadius: BorderRadius.circular(12),
+              //   ),
+              // ),
+            ],
+          ),
+        ),
       ),
-      body: const Center(
-        child: Text('Karakterler Sayfası'),
+    );
+  }
+
+  Widget _searchinputWidget(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 12.0, bottom: 16.0),
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: 'Karakterlerde ara',
+          labelStyle: TextStyle(color: Theme.of(context).colorScheme.onSurface),
+          border: const OutlineInputBorder(
+            borderSide: BorderSide(
+              /* renkler theme cekmek olcusun fala icin */
+            ),
+          ),
+          prefixIcon: const Icon(Icons.search),
+          suffixIcon: IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+        ),
       ),
     );
   }
